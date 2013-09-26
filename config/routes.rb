162e930c93d001d4640f0dca75766318cdbd7dd2
pigats <andrea@fruktarbo.com>
2013-09-26 23:15:15 +0200

    switch routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rr48rails2013::Application.routes.draw do
-  root 'site#landing_page'
+  root 'site#index'
 
   get 'trailer' => 'site#landing_page'
-  get 'secret' => 'site#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

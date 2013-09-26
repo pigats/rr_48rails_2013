b7d4876f48af9d0499119d371c58d8a4f06a107a
pigats <andrea@fruktarbo.com>
2013-09-26 19:36:13 +0200

    refactor fb faces into tableless model

---
+++ app/models/facebook_faces.rb
@@ -0,0 +1,12 @@
+require 'koala'
+
+class FacebookFaces
+
+  def self.all
+    Rails.cache.fetch("FacebookFacesAll", :expires_in => 12.hours) do    
+      oauth = Koala::Facebook::OAuth.new 
+      fb = Koala::Facebook::API.new(oauth.get_app_access_token)
+      fb.get_connections('222866994508573','members').map {|person| {:name => person["name"], :src => fb.get_picture(person["id"], type: 'square')} }
+    end
+  end
+end

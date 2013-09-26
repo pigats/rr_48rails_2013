2182f9e6995a8786c1b619ebec001fb1e82da3d2
pigats <andrea@fruktarbo.com>
2013-09-26 19:37:59 +0200

    update index view and site controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,10 +1,9 @@
-require 'koala'
 
 class SiteController < ApplicationController
   def index
-    oauth = Koala::Facebook::OAuth.new 
-    fb = Koala::Facebook::API.new(oauth.get_app_access_token)
-    @fb_group = fb.get_connections('222866994508573','members').map {|person| {:name => person["name"], :src => fb.get_picture(person["id"], type: 'square')}}
+
+    @faces = FacebookFaces.all
+    @tweets = Tweets.last(3)
 
   end
 

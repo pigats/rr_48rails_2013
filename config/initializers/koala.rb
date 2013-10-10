c80020d2cd8c189e0914a9a506d339b74f7dbd1a
pioz <enrico@megiston.it>
2013-10-10 14:38:25 +0200

    add photos of 48rails 2012

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -4,7 +4,7 @@ Koala::Facebook::OAuth.class_eval do
   def initialize_with_default_settings(*args)
     raise "application id and/or secret are not specified in the envrionment" unless ENV['API_KEY'] && ENV['APP_SECRET']
     initialize_without_default_settings(ENV['API_KEY'].to_s, ENV['APP_SECRET'].to_s, args.first)
-  end 
+  end
 
-  alias_method_chain :initialize, :default_settings 
+  alias_method_chain :initialize, :default_settings
 end

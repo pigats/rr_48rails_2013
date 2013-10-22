1dbbb5153e4203015ca5871fc685be2f186abe2a
pigats <andrea@fruktarbo.com>
2013-10-22 13:15:07 +0200

    add teams webapp links

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -70,6 +70,6 @@ class TeamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
-      params.require(:team).permit(:name, :colour, :order, :prize_id, :hashtag)
+      params.require(:team).permit(:name, :colour, :order, :prize_id, :hashtag, :url)
     end
 end
